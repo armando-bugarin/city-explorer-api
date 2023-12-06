d4e2169ed0e9ac37dd075ee1c08345f7509c31dc
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-12-06 13:05:20 -0800

    add to server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
-const app = express();
-const port = 3001;
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(cors());
+
+// routes
+app.get('/', (request, response) => {
+  response.send('hello world');
+});
+
+// helper functions
+
+// classes
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
